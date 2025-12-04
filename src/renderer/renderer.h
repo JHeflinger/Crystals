d241dfeb7df335d85b2e58cf2e64790c083900e9
Jason Heflinger <jasonheflinger@gmail.com>
2025-12-03 21:32:19 -0500

    fixed

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "renderer/backend.h"
 #include "scene/scene.h"
 #include "renderer/denoise.h"
 #include "renderer/image.h"
@@ -13,11 +12,9 @@ public:
     Image render(std::string filepath, size_t w, size_t h);
     Image render(Scene scene, size_t w, size_t h);
 	bool saveComposites(std::string filepath);
-    std::string GPU() { return m_backend.gpuName(); }
 private:
     void renderPixels(size_t start, size_t count, Image& image, Scene& scene);
 private:
-    VulkanBackend m_backend;
     std::mutex m_mutex;
     int m_counter;
 	int m_threadpool;
